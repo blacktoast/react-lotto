73f625ac04723369e45be06759671e7c0567b44f
blacktoast <dbstjd100@naver.com>
2022-02-10 07:11:53 +0000

    feat: 구입금액체크 구현

--- src/Components/PurchaseForm.jsx
+++ src/Components/PurchaseForm.jsx
@@ -1,8 +1,6 @@
 import React, { useCallback, useState } from "react";
 
 function CommonBtn({ onClick, children }) {
-  const [inputPrice, setInputPrice] = useState("");
-
   return (
     <button
       type="button"
@@ -15,6 +13,19 @@ function CommonBtn({ onClick, children }) {
 }
 
 function PurchaseForm({ onClick, onRef = null }) {
+  const [inputPrice, setInputPrice] = useState("");
+  const [numberTickets, setNumberTickets] = useState(0);
+  const [overPrice, setOverPrice] = useState(0);
+
+  const onChange = (e) => {
+    setInputPrice(e.target.value);
+    if (parseInt(e.target.value) < 30000) {
+      setNumberTickets(Math.floor(parseInt(e.target.value) / 1000));
+    } else {
+      setNumberTickets(0);
+    }
+  };
+
   return (
     <form className="mt-5 mx-10 sm:w-8/12">
       <label className="mb-4  inline-block font-sans font-semibold">
@@ -24,14 +35,22 @@ function PurchaseForm({ onClick, onRef = null }) {
         <input
           ref={onRef}
           type="number"
+          value={inputPrice}
+          onChange={onChange}
           className="w-8/12 mr-2 pl-2 price-input border-8 h-12 shadow-sm shadow-neutral-600 border-cyan-200 "
           placeholder="구입 금액"
         />
         <CommonBtn onClick={onClick}> 자동 구매</CommonBtn>
       </div>
-      <p className="text-sm text-red-500 mx-10 mt-2 mt-0">
-        1000원이상 30000원 이하 금액을 입력해주세요
-      </p>
+      {!numberTickets ? (
+        <p className="text-sm text-red-500 mt-2 text-center">
+          1000원이상 30000원 이하 금액을 입력해주세요
+        </p>
+      ) : (
+        <p className="text-sm text-blue-500 mt-2 text-center">
+          {numberTickets} 개의 티켓을 구입할수있습니다{" "}
+        </p>
+      )}
     </form>
   );
 }
---
+++ src/utils/inputUtils.js
@@ -0,0 +1,4 @@
+export function isEmpty(input) {
+  if (input === "") return true;
+  return false;
+}
